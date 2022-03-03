70b571a534854bc09168509aace20a9429e10f34
RaouiRahmeni <raouirahmeni@gmail.com>
2022-03-03 17:05:12 +0100

    i changed the hole structure of the project

--- auth/validation.js
+++
@@ -1,9 +0,0 @@
-$(".sign_up btn").click(function (input) {
-  var mailFormat = /^\w+([\.-]?\w+)*@([\.-]?\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if (input.value.match(mailFormat)) {
-    return true;
-  } else {
-    alert("You have entered an invalid email");
-    return false;
-  }
-});
