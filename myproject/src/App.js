f68fc38831111d985ed53c89928c4ed584d9fc4b
RaouiRahmeni <raouirahmeni@gmail.com>
2022-03-03 17:01:55 +0100

    i changed the hole structure of the project

---
+++ myproject/src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/home/Home";
+import Login from "./components/login/Login";
+import NavBar from "./components/NavBar/NavBar";
+import Register from "./components/register/Register";
+import Rendezvous from "./components/rendez-vous/RendezVous";
+import Specialités from "./components/specialités/Specialités";
+function App() {
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+
+        <Route path="/login" element={<Login />}></Route>
+
+        <Route path="/rendezvous" element={<Rendezvous />}></Route>
+
+        <Route path="/specialités" element={<Specialités />}></Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ myproject/src/components/specialités/Specialités.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Specialités() {
+  return <div className="container">specialités</div>;
+}
